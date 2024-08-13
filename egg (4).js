2707f4c5a5d80f191918077d2aa24f2338353f87
Audrey <audreyffm@gmail.com>
2024-08-13 17:35:09 -0400

    salt and pepper shakers and some code

---
+++ egg (4).js
@@ -0,0 +1,271 @@
+/*
+@title: egg
+@author: Audrey Lucas
+@tags: ['maze', 'levels']
+@addedOn: 2024-08-13
+
+HOW TO PLAY
+'WASD' to control your player
+'I' to reset the game
+
+*/
+
+const player = "p";
+const eggwhite = "e";
+const wall = "w";
+const portal = "o";
+const salt = "s";
+const pepper = "r";
+
+const melody = tune`
+82.1917808219178: G4-82.1917808219178,
+82.1917808219178: E5-82.1917808219178,
+2465.753424657534`
+const melody2 = tune``
+
+setLegend(
+  [ player, bitmap`
+................
+................
+................
+.....00000......
+....0999990.....
+...099999990....
+..09929992990...
+..09909990990...
+..09909990990...
+..09999999990...
+..09990009990...
+...099999990....
+....0999990.....
+.....00000......
+................
+................` ],
+  [ eggwhite, bitmap`
+...........0000.
+.....00000022220
+....022222222220
+..0022222222220.
+.02222222222220.
+.02222222222220.
+0222222222222220
+0222222222222220
+0222222222222220
+0222222222222220
+0222222222222220
+0222222222222220
+022222222222200.
+.022222222220...
+..0022222200....
+....000000......` ],
+  [ wall, bitmap`
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777
+7777777777777777` ],
+  [ portal, bitmap`
+....000000000...
+..002HHL0HH2000.
+.00L00000200LL00
+00005002555H60L0
+00H2500000005550
+05555555555550H0
+000005H0HHH25050
+00205LL260L0HH00
+05L00L0HLL0H6500
+00L0000LLHL000L0
+0L25205000005550
+0555550555005200
+00050H50000L6000
+..056000550HL00.
+..0005H25550L0..
+...0000000000...` ],
+  [ salt, bitmap`
+................
+......0LL0......
+.....0L00L0.....
+.....000000.....
+......0000......
+.....1....1.....
+.....122221.....
+.....122221.....
+.....122221.....
+....12222221....
+....12222221....
+....12222221....
+....12222221....
+....12222221....
+.....111111.....
+................` ],
+  [ pepper, bitmap`
+................
+......0LL0......
+.....0L00L0.....
+.....000000.....
+......0000......
+.....1....1.....
+.....1CCCC1.....
+.....1CCCC1.....
+.....1CCCC1.....
+....1CCCCCC1....
+....1CCCCCC1....
+....1CCCCCC1....
+....1CCCCCC1....
+....1CCCCCC1....
+.....111111.....
+................` ],
+)
+
+
+let level = 0
+const levels = [
+  map`
+wwwwwwwwwww
+w...w.....w
+p.w...w.w.w
+www.www.w.w
+w...w...wsw
+w.wwwww.w.w
+w.wr.w..www
+w.ww.ww.w.e
+w.....w...w
+wwwwwwwwwww`,
+  map`
+wwwwwwwwwwww
+w.ww...w...w
+w....w...www
+ww.wwwww..ww
+ww.w.w.ww..w
+w..w....ww.w
+w.www.w....w
+w...w.w..www
+w.w.w.wwww.p
+w.w.w......w
+wwwewwwwwwww`,
+  map`
+...........e..
+.p............
+.........o....
+..............
+..............
+..............
+..............
+..............
+..............
+..............
+.....o........
+..............
+..............`,
+  map`
+wwwwwwpwwwwwwwww
+w......w.......w
+w.w.w.wwww.w.w.w
+w.w.w....w.www.w
+www.wwww.w...w.w
+w.w...w..www.w.w
+w.www.w.ww...w.w
+w.w...w.w..w.w.w
+w.w.wwwwww.www.w
+w...w......w...w
+w.www.wwwwww.www
+w.w.....w....w.w
+w.www.w.www.ww.e
+w.....w...w....w
+wwwwwwwwwwwwwwww`,
+  map`
+........
+.e......
+........
+...p....
+........
+........
+........`,
+  map`
+........
+.e......
+........
+...p....
+........
+........
+........`,
+]
+
+const currentLevel = levels[level];
+setMap(levels[level])
+
+
+setSolids([player, wall])
+
+setPushables({
+  [ player ]: []
+})
+
+
+onInput("s", () => {
+  getFirst(player).y += 1
+})
+onInput("w", () => {
+  getFirst(player).y -= 1
+})
+onInput("a", () => {
+  getFirst(player).x -= 1
+})
+onInput("d", () => {
+  getFirst(player).x += 1
+})
+
+
+afterInput(() => {
+  
+  const portalNumber = tilesWith(player).length;
+  const portalCovered = tilesWith(portal, player).length;
+
+  if (portalCovered === portalNumber) {
+    getFirst(player).x = 5;
+    getFirst(player).y = 10;
+  }
+
+  const saltNumber = tilesWith(player).length;
+  const saltCovered = tilesWith(salt, player).length;
+
+  if (saltCovered === saltNumber) {
+    salt.remove()
+    playTune(melody)
+    
+  }
+
+  const pepperNumber = tilesWith(player).length;
+  const pepperCovered = tilesWith(pepper, player).length;
+
+  if (pepperCovered === pepperNumber) {
+    pepper.remove()
+    playTune(melody)
+  }
+  
+  const targetNumber = tilesWith(eggwhite).length;
+  const numberCovered = tilesWith(eggwhite, player).length;
+ 
+  if (numberCovered === targetNumber) {
+    level = level + 1;
+    const currentLevel = levels[level];
+    
+    if (currentLevel !== undefined) {
+      setMap(currentLevel);
+    } else {
+      addText("you win!", { y: 4, color: color`3` });
+    }
+  }
+});
+
